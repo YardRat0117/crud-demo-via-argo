10fdcde56f0ea9cb37146b8c54968844776a44d7
YardRat <yardrat0117@gmail.com>
2025-11-09 10:20:45 +0800

    feat: Web UI available now

--- initEnv.sh
+++ initEnv.sh
@@ -17,3 +17,21 @@ kubectl create clusterrolebinding argo-workflow-binding \
 
 # Install Argo Workflows
 kubectl apply -n argo -f https://github.com/argoproj/argo-workflows/releases/latest/download/install.yaml
+
+# Patch argo-server to run without auth & HTTPS
+kubectl -n argo patch deploy argo-server \
+  --type=json \
+  -p='[
+    {"op": "replace", "path": "/spec/template/spec/containers/0/args",
+     "value": ["server", "--auth-mode=server", "--secure=false"]},
+    {"op": "replace", "path": "/spec/template/spec/containers/0/readinessProbe",
+     "value": {
+       "httpGet": {
+         "path": "/",
+         "port": 2746,
+         "scheme": "HTTP"
+       },
+       "initialDelaySeconds": 5,
+       "periodSeconds": 5
+     }}
+  ]'
